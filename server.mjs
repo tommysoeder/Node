cfce1e0195addc09506b703376e37402ccd15364
tommysoeder <148865051+tommysoeder@users.noreply.github.com>
2024-03-07 11:25:44 +0100

    Add files via upload

--- server.mjs
+++ server.mjs
@@ -5,9 +5,11 @@ const server = createServer((request, response)=>{
 
     response.statusCode = 200
 
-    response.setHeader("Content-Type", "text/html")
+    response.setHeader("Content-Type", "application/json")
 
-    response.end("<html><body><h1>Texto reemplazado</h1><body><html>")
+    const jsonResponseBody = JSON.stringify({location: "Mars"})
+
+    response.end(jsonResponseBody)
 })
 
 server.listen(3000,()=>{
